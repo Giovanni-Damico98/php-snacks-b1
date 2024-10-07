b3b51d04b8d1a097699e53027f732c1fd2069eda
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-10-07 15:01:02 +0200

    Snack 4a-b-c-d

--- index.php
+++ index.php
@@ -1,70 +1,302 @@
- <!-- Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo 
-  nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia 
-  un numero. Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" -->
- <?php
-$nameIsOk = false;
-$emailIsOk = false;
-$ageIsOk = false;
-// Prendo i dati inseriti
-$nome = trim($_GET['nome']);
-$email = trim($_GET['email']);
-$age = trim($_GET['eta']);
-// Verifico che abbia almeno 4 caratteri
-if (strlen($nome) > 3) {
-    $nameIsOk = true;
-} else {
-    $nameIsOk = false;
-// Email
-}
-if (strpos($email, '@') !== false && strpos($email, '.') !== false) {
-    $emailIsOk = true;
-} else {
-    $emailIsOk = false;
-}
-// Età
-if (is_numeric($age)) {
-    $ageIsOk = true;
-} else {
-    $ageIsOk = false;
-}
-
-
+<?php
+$apiKey = 'LwhKikJGS81UomSswFdVDzBA7hd9C4YHukP3FBpC1r4';
+$url = 'https://api.unsplash.com/';
+$classi = [
+    "Classe 1A" => [
+        [
+            "id" => 1,
+            "nome" => "Marco",
+            "cognome" => "Rossi",
+            "anni" => 23,
+            "voto_medio" => 8.5,
+            "linguaggio_preferito" => "PHP",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 2,
+            "nome" => "Luca",
+            "cognome" => "Verdi",
+            "anni" => 27,
+            "voto_medio" => 3.5,
+            "linguaggio_preferito" => "JS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 3,
+            "nome" => "Giulia",
+            "cognome" => "Bianchi",
+            "anni" => 23,
+            "voto_medio" => 5.5,
+            "linguaggio_preferito" => "CSS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 4,
+            "nome" => "Sofia",
+            "cognome" => "Russo",
+            "anni" => 27,
+            "voto_medio" => 8.0,
+            "linguaggio_preferito" => "HTML",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 5,
+            "nome" => "Andrea",
+            "cognome" => "Ferrari",
+            "anni" => 22,
+            "voto_medio" => 7.5,
+            "linguaggio_preferito" => "PHP",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+    ],
+    "Classe 2A" => [
+        [
+            "id" => 27,
+            "nome" => "Francesca",
+            "cognome" => "Ricci",
+            "anni" => 27,
+            "voto_medio" => 9.0,
+            "linguaggio_preferito" => "JS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 22,
+            "nome" => "Matteo",
+            "cognome" => "Moretti",
+            "anni" => 23,
+            "voto_medio" => 4.3,
+            "linguaggio_preferito" => "HTML",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 30,
+            "nome" => "Alessandro",
+            "cognome" => "Lombardi",
+            "anni" => 27,
+            "voto_medio" => 5.4,
+            "linguaggio_preferito" => "CSS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 33,
+            "nome" => "Elena",
+            "cognome" => "Conti",
+            "anni" => 22,
+            "voto_medio" => 8.4,
+            "linguaggio_preferito" => "PHP",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 20,
+            "nome" => "Valeria",
+            "cognome" => "Gallo",
+            "anni" => 27,
+            "voto_medio" => 9.5,
+            "linguaggio_preferito" => "JS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+    ],
+    "Classe 3A" => [
+        [
+            "id" => 31,
+            "nome" => "Simone",
+            "cognome" => "Fabbri",
+            "anni" => 22,
+            "voto_medio" => 6.9,
+            "linguaggio_preferito" => "CSS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 32,
+            "nome" => "Chiara",
+            "cognome" => "Riva",
+            "anni" => 27,
+            "voto_medio" => 5.3,
+            "linguaggio_preferito" => "HTML",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 33,
+            "nome" => "Lorenzo",
+            "cognome" => "Orlando",
+            "anni" => 22,
+            "voto_medio" => 8.2,
+            "linguaggio_preferito" => "PHP",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 34,
+            "nome" => "Marta",
+            "cognome" => "Martini",
+            "anni" => 22,
+            "voto_medio" => 4.8,
+            "linguaggio_preferito" => "JS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 35,
+            "nome" => "Giovanni",
+            "cognome" => "Giuliani",
+            "anni" => 27,
+            "voto_medio" => 8.1,
+            "linguaggio_preferito" => "CSS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+    ],
+    "Classe 4A" => [
+        [
+            "id" => 46,
+            "nome" => "Silvia",
+            "cognome" => "De Luca",
+            "anni" => 30,
+            "voto_medio" => 4.2,
+            "linguaggio_preferito" => "PHP",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 47,
+            "nome" => "Federico",
+            "cognome" => "Costa",
+            "anni" => 30,
+            "voto_medio" => 5.1,
+            "linguaggio_preferito" => "HTML",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 48,
+            "nome" => "Michele",
+            "cognome" => "Cattaneo",
+            "anni" => 22,
+            "voto_medio" => 3.7,
+            "linguaggio_preferito" => "JS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 49,
+            "nome" => "Anna",
+            "cognome" => "Serra",
+            "anni" => 30,
+            "voto_medio" => 8.6,
+            "linguaggio_preferito" => "CSS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 50,
+            "nome" => "Antonio",
+            "cognome" => "Cervi",
+            "anni" => 30,
+            "voto_medio" => 4.0,
+            "linguaggio_preferito" => "PHP",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+    ],
+    "Classe 5A" => [
+        [
+            "id" => 61,
+            "nome" => "Davide",
+            "cognome" => "Ferrero",
+            "anni" => 33,
+            "voto_medio" => 8.7,
+            "linguaggio_preferito" => "JS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 62,
+            "nome" => "Sara",
+            "cognome" => "Leoni",
+            "anni" => 33,
+            "voto_medio" => 4.1,
+            "linguaggio_preferito" => "PHP",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 63,
+            "nome" => "Daniele",
+            "cognome" => "Bertoni",
+            "anni" => 33,
+            "voto_medio" => 4.4,
+            "linguaggio_preferito" => "HTML",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 64,
+            "nome" => "Giada",
+            "cognome" => "Ragusa",
+            "anni" => 30,
+            "voto_medio" => 5.0,
+            "linguaggio_preferito" => "CSS",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+        [
+            "id" => 65,
+            "nome" => "Claudio",
+            "cognome" => "Lanza",
+            "anni" => 30,
+            "voto_medio" => 6.7,
+            "linguaggio_preferito" => "PHP",
+            "immagine" => "https://source.unsplash.com/random/200x200?person",
+        ],
+    ],
+];
 ?>
 
- <!DOCTYPE html>
- <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Snack b1</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1pFkwJp4ornC0w" crossorigin="anonymous">
- </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+</head>
 
- <body>
-     <form method="GET" action="">
-         <!-- Campo Nome -->
-         <div class="mb-3">
-             <label for="nome" class="form-label">Nome</label>
-             <input type="text" class="form-control" id="nome" placeholder="Inserisci il tuo nome">
-         </div>
+<body>
+    <div class="container">
 
-         <!-- Campo Email -->
-         <div class="mb-3">
-             <label for="email" class="form-label">Email</label>
-             <input type="email" class="form-control" id="email" placeholder="Inserisci la tua email">
-         </div>
+        <table class="table table-striped">
+            <!-- Voci della tabella -->
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Cognome</th>
+                    <th>Anni</th>
+                    <th>Voto Medio</th>
+                    <th>Linguaggio Preferito</th>
+                    <th>Immagine</th>
+                </tr>
+            </thead>
+            <!-- Corpo della tabella -->
+            <tbody>
+                <!-- Per ogni classe nell'array classi eseguo un ciclo che mi permetta di vedere ogni classe -->
+                <?php foreach ($classi as $classe) { ?>
+                    <!-- Per ogni classe eseguo un ciclo che mi permetta di vedere ogni studente con i relativi campi -->
+                    <?php foreach ($classe as $studente) { ?>
+                        <!-- Table row -->
+                        <tr>
+                            <!-- Table Data + codice php per mostrare in pagina i campi -->
+                            <td><?php
+                                echo $studente['id']; ?></td>
+                            <td><?php
+                                echo $studente['nome']; ?></td>
+                            <td><?php
+                                echo $studente['cognome']; ?></td>
+                            <td><?php
+                                echo $studente['anni']; ?></td>
+                            <td><?php
+                                echo $studente['voto_medio']; ?></td>
+                            <td><?php
+                                echo $studente['linguaggio_preferito']; ?></td>
+                            <td><img url="<?php
+                                            echo $studente['immagine']; ?>" alt=""></td>
+                        </tr>
+                    <?php } ?>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</body>
 
-         <!-- Campo Età -->
-         <div class="mb-3">
-             <label for="eta" class="form-label">Età</label>
-             <input type="number" class="form-control" id="eta" placeholder="Inserisci la tua età">
-         </div>
-
-         <!-- Bottone per inviare il form -->
-         <button type="submit" class="btn btn-primary" name="invia">Invia</button>
-     </form>
- </body>
-
- </html>
+</html>
